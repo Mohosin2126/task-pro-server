86c34b637fa3bf09720f9d04529eec096d0b09ae
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-25 03:50:58 +0800

    added new get method and update

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express= require('express')
-const app=express()
-const cors=require('cors')
-const port=process.env.PORT||5000
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
-
-
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crat2tn.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -31,21 +25,58 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
-    const taskCollection=client.db('taskDb').collection('alltask')
-
+    const taskCollection = client.db('taskDb').collection('alltask');
 
 
+    app.get('/alltask',async(req,res)=>{
+        const cursor = taskCollection.find();
+          const result = await cursor.toArray();
+          res.send(result);
+    })
 
 
-    app.post("/alltask",async(req,res)=>{
-        const task=req.body
-        const result=await taskCollection.insertOne(task)
-        res.send(result)
-      })
-      
+
+    app.post("/alltask", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
 
 
+
+
+    app.get("/alltask", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+
+   
+  
+   
+
+    app.patch('/alltask/:id', async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          title: item.title,
+          description: item.description
+        }
+      };
+
+      const result = await taskCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/alltask/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -57,14 +88,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send('task pro is running');
+});
 
-
-
-
-app.get("/",(req,res)=>{
-    res.send('task pro is running')
-})
-
-app.listen(port,()=>{
-    console.log(`taskpro  is running in port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`taskpro is running in port ${port}`);
+});
