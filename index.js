5dfbfe28eac43799b14b3ca33b8adc9db9ad8476
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-24 23:03:24 +0800

    created a collection in database and post the data in mongodb

--- index.js
+++ index.js
@@ -30,6 +30,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const taskCollection=client.db('taskDb').collection('alltask')
+
+
+
+
+
+    app.post("/alltask",async(req,res)=>{
+        const task=req.body
+        const result=await taskCollection.insertOne(task)
+        res.send(result)
+      })
+      
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
